11efbb128b89e55042471c1e3d41d45217544649
endorphinism <chavanat_pongy@hotmail.com>
2013-10-16 14:04:31 +0700

    add new file

---
+++ src/test/New1.java
@@ -0,0 +1,14 @@
+package test;
+
+
+public class New1 {
+	public static String word2;
+
+	public static void main(String[] args) {
+	
+		word2 = "hello world";
+	System.out.println(word2);
+	
+	}
+
+}
